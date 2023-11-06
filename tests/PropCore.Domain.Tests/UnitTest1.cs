c62c79ecbd6ee94c036aee3bd39f2e40418ba8f0
George Arthur <Arthurgeorgekojo@gmail.com>
2023-11-06 09:00:00 +0000

    test: add xUnit test project scaffolding

---
+++ tests/PropCore.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace PropCore.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/PropCore.Infrastructure.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace PropCore.Infrastructure.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
